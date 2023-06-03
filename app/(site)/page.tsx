f8edcd0dde5b0cf3126739ce4cb1038404d74d48
ssabirovo <muhiddinsobirov957@gmail.com>
2023-06-03 22:57:07 +0500

    feat: added Input component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
@@ -8,14 +9,14 @@ export default function Home() {
           alt="logo"
           height={48}
           width={48}
-          className="mx-auto w-aut"
+          className="mx-auto w-auto"
           src="/images/logo (1).png"
         />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
           sign in to your account
         </h2>
       </div>
-      {/* AuthForm */}
+      <AuthForm />
     </div>
   );
 }
